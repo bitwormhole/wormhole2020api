fb341cee1b657b145b9aa1b8daae109dd35aa9d5
xukun <xukun17@sina.com>
2021-05-26 22:38:55 +0800

    添加了一些注释

--- api/v1/actions/auth_api.go
+++ api/v1/actions/auth_api.go
@@ -2,6 +2,9 @@ package actions
 
 import "github.com/bitwormhole/wormhole2020api/api/v1/vo"
 
+// path=/api/v1/auth
 type IAuth interface {
+
+	// method=POST
 	Login(param *vo.Auth) (*vo.Auth, error)
 }
